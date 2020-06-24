4613c345ea9c0df070f84198d196bfd58550fd90
Beatriz Monteiro <monteiro.beatriz70@gmail.com>
2020-06-24 16:43:16 -0300

    comments and normalization

--- Image_Restoration.py
+++ Image_Restoration.py
@@ -17,26 +17,8 @@ Short Assignment 2 :  Image Restoration
 """
 
 import numpy as np
-import imageio, math, warnings
+import imageio, warnings
 import scipy.ndimage, scipy.fftpack
-
-#CREATES A BORDER OF n/2 0s around the img n=3 t=3 a = b = som = 1
-def padding(f, kernel):
-    
-    n, m = f.shape
-    
-    kr, kc = kernel.shape
-    
-    centerR = int((kr-1)/2) # find middle row of kernel
-    centerC = int((kc-1)/2) #find middle collun of kernel
-
-    pad_filter = np.zeros((n+(kr-1),m+(kc-1)), dtype=np.float)
-
-    for i in range(centerR, n+centerR):
-        for j in range(centerC,m+centerC):
-            pad_filter[i][j] = f[i-centerR][j-centerC]
-    
-    return pad_filter
 
 def gaussian_filter(k, sigma):
    arx = np.arange((-k // 2) + 1.0, (k // 2) + 1.0)
@@ -81,6 +63,14 @@ laplacian = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 padding_filter = (img.shape[0]//2)-gfilter.shape[0]//2
 gfilter_pad = np.pad(gfilter, (padding_filter, padding_filter-1), "constant",  constant_values=0)
 
+#sets padding for laplacian filter -> used in constrained least squares
+padding_laplace = (img.shape[0]//2)-laplacian.shape[0]//2
+laplacian_pad = np.pad(laplacian, (padding_laplace, padding_laplace-1), "constant",  constant_values=0)
+laplacian_pad = np.fft.ifft2(laplacian_pad)
+
+#START OF RESTORATION:
+
+#DENOISING
 # applying fast fourier transform in img and gaussian filter for denoising 
 img_fft = np.fft.ifft2(img)
 gfilter_fft = np.fft.ifft2(gfilter_pad)
@@ -90,26 +80,24 @@ transformed_img = np.multiply(gfilter_fft, img_fft)
 transformed_img_ifft = np.real(scipy.fftpack.fftshift(scipy.fftpack.ifftn(transformed_img)))
 
 #normalizing
-max_img = np.amax(img)
-min_t = np.amin(transformed_img)
+max_img = np.max(img)
+min_t = np.min(transformed_img)
 
 transformed_img = (transformed_img_ifft-min_t)*max_img/(max_img-min_t)
 transformed_img = scipy.fftpack.fftn(transformed_img)
+#end of denoising
 
-#sets padding for laplacian filter
-padding_laplace = (img.shape[0]//2)-laplacian.shape[0]//2
-laplacian_pad = np.pad(laplacian, (padding_laplace, padding_laplace-1), "constant",  constant_values=0)
-laplacian_pad = np.fft.ifft2(laplacian_pad)
-
+#DEBLURING
+#resets blur using contraied least squares method
 final_img = constrained_least_squares(gfilter_fft, transformed_img, laplacian_pad, gamma, final_img)
 
 #inverse transform and normalize
 final_img = np.real(scipy.fftpack.fftshift(scipy.fftpack.ifftn(final_img)))
 
-max_f = np.amax(final_img)
-min_f = np.amin(final_img)
+#normalizing
+max_f = np.max(final_img)
+min_f = np.min(final_img)
 final_img = (final_img-min_f)*max_img/(max_f-min_f)
-
         
 #prints standard deviation of image after restoration
 print("%.1f" % np.std(final_img)) 
